1700337ba0fa942d92cffc2881f47fda1ce0936b
alasaidi <alasaidi55gmail.com>
2024-05-21 19:10:04 +0200

    api calls exercises

--- 07-api-calls/pokemon/exercises/pokemon-in-order.js
+++ 07-api-calls/pokemon/exercises/pokemon-in-order.js
@@ -1,5 +1,5 @@
 import { ORIGIN } from '../config.js';
-
+import fetch from 'node-fetch';
 /**
  * Returns a selected array of pokemon in order.
  *
@@ -10,31 +10,32 @@ import { ORIGIN } from '../config.js';
  *
  * @throws {Error} HTTP error! status: {number}.
  */
-export const pokemonInOrder = async () => {
+export const pokemonInOrder = async (limit = 10, offset = 10) => {
     // --- generate and declare your resource's URL ---
     // docs: https://pokeapi.co/docs/v2#resource-listspagination-section
-    const URL = _;
+    const URL = `${ORIGIN}/ability/?limit=${limit}&offset=${offset}`;
+    try {
+        // --- fetch the API data (this works!) ---
+        const encodedURL = encodeURI(URL);
+        const response = await fetch(encodedURL);
+        const data = await response.json();
 
-    // --- fetch the API data (this works!) ---
-    const encodedURL = encodeURI(URL);
-    const response = await fetch(encodedURL);
+        // --- process the fetched data (if necessary) ---
+        //  you do not need to use `await` below this comment
+        //  you can refactor this to a separate logic function and test it
+        const pokemon = data;
+        /* --- parse the data if the response was ok (this works!) ---*/
 
-    // --- throw an error if the response is not ok (this works!) ---
-    if (!response.ok) {
+        // --- return the final data ---
+        // return pokemon;
+        console.log(pokemon);
+    } catch {
+        // --- throw an error if the response is not ok (this works!) ---
+
         const message = response.statusText
             ? `${response.status}: ${response.statusText}\n-> ${URL}`
             : `HTTP error! status: ${response.status}\n-> ${URL}`;
         throw new Error(message);
     }
-
-    /* --- parse the data if the response was ok (this works!) ---*/
-    const data = await response.json();
-
-    // --- process the fetched data (if necessary) ---
-    //  you do not need to use `await` below this comment
-    //  you can refactor this to a separate logic function and test it
-    const pokemon = _;
-
-    // --- return the final data ---
-    return pokemon;
 };
+await pokemonInOrder(10, 10);
